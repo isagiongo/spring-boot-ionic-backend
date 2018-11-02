54ac81e8c8d5632afd99bf9d386f2d31ceaab42c
Isa Giongo <isagiongo@yahoo.com.br>
2018-11-02 10:18:49 -0300

    Instancia categorias

--- src/main/java/com/isagiongo/cursomc/CursomcApplication.java
+++ src/main/java/com/isagiongo/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.isagiongo.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.isagiongo.cursomc.domain.Categoria;
+import com.isagiongo.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Papelaria");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 }
