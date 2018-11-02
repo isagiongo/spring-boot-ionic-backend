92deffd846585576b729251e07920d386ae4c416
Isa Giongo <isagiongo@yahoo.com.br>
2018-11-02 15:40:25 -0300

    Associa produtos às categorias

--- src/main/java/com/isagiongo/cursomc/CursomcApplication.java
+++ src/main/java/com/isagiongo/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.isagiongo.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.isagiongo.cursomc.domain.Categoria;
+import com.isagiongo.cursomc.domain.Produto;
 import com.isagiongo.cursomc.repositories.CategoriaRepository;
+import com.isagiongo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +30,22 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		Categoria cat3 = new Categoria(null,"Papelaria");
+
+		Produto p1 = new Produto(null,"Computador", 2450.00);
+		Produto p2 = new Produto(null,"Cadeira", 324.98);
+		Produto p3 = new Produto(null,"Mouse", 29.90);
+		Produto p4 = new Produto(null,"Papel A4", 9.40);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p4.getCategorias().addAll(Arrays.asList(cat3));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
 	}
 }
