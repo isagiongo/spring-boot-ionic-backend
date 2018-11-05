6148920333b07dc764550d0ceda948cd312b1764
Isa Giongo <isagiongo@yahoo.com.br>
2018-11-05 21:23:28 -0200

    Lista todas categorias e cria DTO para categorias

--- src/main/java/com/isagiongo/cursomc/services/CategoriaService.java
+++ src/main/java/com/isagiongo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.isagiongo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Categoria possui produtos. Não é possível excluir.");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
