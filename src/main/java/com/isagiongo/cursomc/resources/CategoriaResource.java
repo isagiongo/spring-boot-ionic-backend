ab6c7eb47453864b1faf4191a573f60e5f3ca868
Isa Giongo <isagiongo@yahoo.com.br>
2018-11-10 09:33:29 -0200

    Cria métodos PUT, Find, Delete para cliente

--- src/main/java/com/isagiongo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/isagiongo/cursomc/resources/CategoriaResource.java
@@ -34,13 +34,6 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List<Categoria> lista = service.findAll();
-		List<CategoriaDTO> listaDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listaDTO);
-	}
-
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
 		Categoria obj = service.fromDTO(objDTO);
@@ -63,14 +56,20 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> lista = service.findAll();
+		List<CategoriaDTO> listaDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);
+	}
+
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Categoria> lista = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDTO> listaDTO = lista.map(obj -> new CategoriaDTO(obj));
-		return ResponseEntity.ok().body(listaDTO);	}
-
+		return ResponseEntity.ok().body(listaDTO);
+	}
 }
