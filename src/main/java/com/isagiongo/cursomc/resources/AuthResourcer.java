b78c8024e22d53f2a47688e5c20b55e9599e3f95
Isa Giongo <isagiongo@yahoo.com.br>
2018-11-21 20:17:38 -0200

    Renova o token

---
+++ src/main/java/com/isagiongo/cursomc/resources/AuthResourcer.java
@@ -0,0 +1,29 @@
+package com.isagiongo.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isagiongo.cursomc.security.JWTUtil;
+import com.isagiongo.cursomc.security.UserSpringSecurity;
+import com.isagiongo.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResourcer {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
