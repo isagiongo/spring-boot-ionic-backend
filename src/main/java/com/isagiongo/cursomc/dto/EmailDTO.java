d3723a0f8370ef30e536f09eac8e73723b2858ce
Isa GB <isagiongo@yahoo.com.br>
2018-12-16 09:48:39 -0200

    Implementa geração de nova senha para user

---
+++ src/main/java/com/isagiongo/cursomc/dto/EmailDTO.java
@@ -0,0 +1,25 @@
+package com.isagiongo.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty(message = "O campo não pode ser vazio.")
+	@Email(message = "Email inválido.")
+	private String email;
+
+	public EmailDTO() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/isagiongo/cursomc/resources/AuthResource.java
+++ src/main/java/com/isagiongo/cursomc/resources/AuthResource.java
@@ -1,23 +1,30 @@
 package com.isagiongo.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isagiongo.cursomc.dto.EmailDTO;
 import com.isagiongo.cursomc.security.JWTUtil;
 import com.isagiongo.cursomc.security.UserSpringSecurity;
+import com.isagiongo.cursomc.services.AuthService;
 import com.isagiongo.cursomc.services.UserService;
 
 @RestController
 @RequestMapping(value = "/auth")
-public class AuthResourcer {
+public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -26,4 +33,11 @@ public class AuthResourcer {
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+	
 }
