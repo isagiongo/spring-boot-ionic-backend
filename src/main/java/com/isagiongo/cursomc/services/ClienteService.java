5ca16b9fd83e795fa9ddc182b444c739a5310d0e
Isa GB <isagiongo@yahoo.com.br>
2018-12-22 16:13:55 -0200

    Cria endpoint para envio imagem

--- src/main/java/com/isagiongo/cursomc/services/ClienteService.java
+++ src/main/java/com/isagiongo/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.isagiongo.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.isagiongo.cursomc.domain.Cidade;
 import com.isagiongo.cursomc.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSpringSecurity user = UserService.authenticated();
@@ -103,5 +108,9 @@ public class ClienteService {
 			cli.getTelefones().add(objDTO.getTelefone3());
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
--- src/main/java/com/isagiongo/cursomc/services/S3Service.java
+++ src/main/java/com/isagiongo/cursomc/services/S3Service.java
@@ -1,16 +1,24 @@
 package com.isagiongo.cursomc.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.management.RuntimeErrorException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
@@ -24,17 +32,26 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String localFilePath) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			File file = new File(localFilePath);
-			LOG.info("Upload iniciado.");
-			s3cliente.putObject(new PutObjectRequest(bucketName, "teste.jpg", file));
-			LOG.info("Upload finalizado com sucesso.");
-		} catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status code: " + e.getErrorCode());
-		} catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: "+ e.getMessage());
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is;
+			is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
+		}
+	}
+
+	private URI uploadFile(InputStream is, String fileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			s3cliente.putObject(bucketName, fileName, is, meta);
+			return s3cliente.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI");
 		}
 	}
 }
